aab1026f370f22e4a1e6879025c68df59e049165
Dev Harwani <devharwani.work@gmail.com>
2025-06-24 13:57:10 +0530

    add clap and introduce commands enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "ss")]
+#[command(about = "SecretStore - Your terminal-based password manager", long_about = None)]
+
+struct Cli {
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    // initialise secretstore
+    Init,
+    
+    // add details for a platform
+    Add {
+        platform: String,
+        username: String,
+        password: String
+    },
+    // list all the platforms
+    List,
+
+    // get a specfic platform details
+    Get {
+        platform: String
+    },
+
+    // delete a specific platform details
+    Delete {
+        platform: String
+    }
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    
 }
